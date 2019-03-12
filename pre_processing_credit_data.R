c4563fe95ee20c7f9ad04f67ba6e051afb63031b
Thales Raymond <thalesfraymond@gmail.com>
2019-03-12 18:24:34 -0300

    fix comments and NA values :)

--- pre_processing_credit_data.R
+++ pre_processing_credit_data.R
@@ -6,10 +6,16 @@ summary(base)
 
 invalid_age = base[base$age < 0 & !is.na(base$age), ]
 
-#exclude rows with negative ages
+# fix rows with negative ages
 
 filtered_base_by_row = base[base$age >0, ]
 
 age_mean = mean(filtered_base_by_row$age, na.rm = TRUE)
 
 base$age = ifelse(base$age < 0, age_mean, base$age)
+
+# fix rows with NA ages
+
+rows_with_na_age = base[is.na(base$age), ]
+
+base$age = ifelse(is.na(base$age), age_mean, base$age)
