d7fd14f18c581af8403946b68664efb57c1157ae
Thales Raymond <thalesfraymond@gmail.com>
2019-04-17 12:29:11 -0300

    svm regression health insurance

--- regression/svm_regression_health_insurance.R
+++ regression/svm_regression_health_insurance.R
@@ -2,21 +2,19 @@ base = read.csv('health_insurance.csv')
 
 library(e1071)
 
-regressor = rpart(formula = custo ~ idade, data = base, control = rpart.control(minsplit = 3))
+regressor = svm(formula = custo ~ idade, data = base, type = 'eps-regression', kernel = 'radial')
 
 summary(regressor)
 
-tree_predict = predict(regressor, newdata = base[-2])
+svm_predict = predict(regressor, newdata = base[-2])
+
+library(miscTools)
+
+cc = rSquared(base$custo, resid = base$custo - svm_predict)
+
+
 
 library(ggplot2)
 
 ggplot() + geom_point(aes(x = base$idade, y = base$custo), colour = 'blue') + 
-  geom_line(aes(x = base$idade, y = tree_predict), colour = 'red')
-
-
-x_test = seq(min(base$idade), max(base$idade), 0.1)
-
-test_predict = predict(regressor, newdata = data.frame(idade = x_test))
-
-ggplot() + geom_point(aes(x = base$idade, y = base$custo), colour = 'blue') + 
-  geom_line(aes(x = x_test, y = test_predict), colour = 'red')
+  geom_line(aes(x = base$idade, y = svm_predict), colour = 'red')
