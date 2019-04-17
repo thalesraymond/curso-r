cf78bfebe36c1157353ca853fdc6cf86ddd38f1b
Thales Raymond <thalesfraymond@gmail.com>
2019-04-17 12:18:22 -0300

    split folder for regression codes ;)

---
+++ regression/svm_regression_health_insurance.R
@@ -0,0 +1,22 @@
+base = read.csv('health_insurance.csv')
+
+library(e1071)
+
+regressor = rpart(formula = custo ~ idade, data = base, control = rpart.control(minsplit = 3))
+
+summary(regressor)
+
+tree_predict = predict(regressor, newdata = base[-2])
+
+library(ggplot2)
+
+ggplot() + geom_point(aes(x = base$idade, y = base$custo), colour = 'blue') + 
+  geom_line(aes(x = base$idade, y = tree_predict), colour = 'red')
+
+
+x_test = seq(min(base$idade), max(base$idade), 0.1)
+
+test_predict = predict(regressor, newdata = data.frame(idade = x_test))
+
+ggplot() + geom_point(aes(x = base$idade, y = base$custo), colour = 'blue') + 
+  geom_line(aes(x = x_test, y = test_predict), colour = 'red')
