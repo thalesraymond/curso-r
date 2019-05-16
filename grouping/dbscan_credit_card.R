f336ebaa3fc567c3196323c8ea7a7ebce5ac94b3
Thales <thalesfraymond@gmail.com>
2019-05-16 20:05:23 -0300

    dbscan

--- grouping/dbscan_credit_card.R
+++ grouping/dbscan_credit_card.R
@@ -1,7 +1,7 @@
 base = read.csv('credit-card-clients.csv', header = TRUE)
 base$BILL_TOTAL = base$BILL_AMT1 + base$BILL_AMT2 + base$BILL_AMT3 + base$BILL_AMT4 + base$BILL_AMT5 + base$BILL_AMT6
 
-X = data.frame(limit = base$LIMIT_BAL, expenses  = base$BILL_TOTAL)
+X = data.frame(limit = base$LIMIT_BAL, expenses   = base$BILL_TOTAL)
 X = scale(X)
 
 library(dbscan)
