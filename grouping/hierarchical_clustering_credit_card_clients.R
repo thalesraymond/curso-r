55d02756825ba83e4e4d9953561b5e57fadf2622
Thales <thalesfraymond@gmail.com>
2019-05-16 19:36:47 -0300

    ...

--- grouping/hierarchical_clustering_credit_card_clients.R
+++ grouping/hierarchical_clustering_credit_card_clients.R
@@ -1,4 +1,5 @@
-base = read.csv('credit_card_clients.csv', header = TRUE)
+base = read.csv('credit-card-clients.csv', header = TRUE)
+
 base$BILL_TOTAL = base$BILL_AMT1 + base$BILL_AMT2 + base$BILL_AMT3 + base$BILL_AMT4 + base$BILL_AMT5 + base$BILL_AMT6
 
 X = data.frame(limite = base$LIMIT_BAL, gasto = base$BILL_TOTAL)
