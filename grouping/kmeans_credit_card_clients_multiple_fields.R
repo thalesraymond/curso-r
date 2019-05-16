644ca07163c0bfa778f48654cb3940e9ccbf5e72
Thales <thalesfraymond@gmail.com>
2019-05-16 19:22:44 -0300

    kmeans with multiple fields 2

--- grouping/kmeans_credit_card_clients_multiple_fields.R
+++ grouping/kmeans_credit_card_clients_multiple_fields.R
@@ -1,3 +1,31 @@
 base = read.csv('credit-card-clients.csv')
 
 base$BILL_TOTAL = base$BILL_AMT1 + base$BILL_AMT2 + base$BILL_AMT3 + base$BILL_AMT4 + base$BILL_AMT5 + base$BILL_AMT6
+
+X = data.frame(limite= base$LIMIT_BAL
+               ,gasto = base$BILL_TOTAL
+               ,genero = base$SEX
+               ,educacao = base$EDUCATION
+               ,civil = base$MARRIAGE
+               ,idade = base$AGE)
+
+X = scale(X)
+
+set.seed(1)
+
+wcss = vector()
+
+for(i in 1:10){
+  kmeans = kmeans(x = X, centers = i)
+  wcss[i] = sum(kmeans$withinss)
+}
+
+plot(1:10, wcss, type = 'b', xlab = 'Clusters', ylab = 'WCSS')
+
+set.seed(1)
+
+kmeans = kmeans(x = X, centers = 4)
+
+predict = kmeans$cluster
+
+pairs(X, col = c(1:4)[predict])
